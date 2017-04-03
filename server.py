24b9114a85f104b58178ba162d12017f8b826c36
Benjamin <bencommet@gmail.com>
2017-04-02 20:42:38 -0400

    Working with websockets, kill me

--- server.py
+++ server.py
@@ -5,9 +5,35 @@ import threading
 import signal
 import sys
 import flask
-# from flask import Flask, render_template
-# from flask_socketio import send, emit
+from flask import Flask, render_template
+from flask.ext.socketio import SocketIO, emit
 
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret!'
+socketio = SocketIO(app)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@socketio.on('my event', namespace='/test')
+def test_message(message):
+    emit('my response', {'data': message['data']})
+
+@socketio.on('my broadcast event', namespace='/test')
+def test_message(message):
+    emit('my response', {'data': message['data']}, broadcast=True)
+
+@socketio.on('connect', namespace='/test')
+def test_connect():
+    emit('my response', {'data': 'Connected'})
+
+@socketio.on('disconnect', namespace='/test')
+def test_disconnect():
+    print('Client disconnected')
+
+if __name__ == '__main__':
+    socketio.run(app)
 # Handle the command line arguments
 if len(sys.argv) != 3:
     print('incorrect number of arguments')
@@ -18,9 +44,13 @@ try:
     maxMoles = int(sys.argv[2])
 except ValueError:
     print('Your command line arguments should be input as follows <number of moles> <max moles at at time>\n')
+
+threads = []
 def signal_handler(signal, frame):
     print('You exited the program\n')
     sys.exit(0)
+    for i in threads:
+        i.exit()
 
 
 signal.signal(signal.SIGINT, signal_handler)
@@ -40,7 +70,6 @@ def moleDown(moleNum):
     semaphore.release()
     randSleep(moleNum)
 
-# threads = []
 # for i in range(numberOfMoles):
 #     thread = threading.Thread(target=randSleep, args =(i,))
 #     threads.append(thread)
---
+++ websocket.js
@@ -0,0 +1,12 @@
+    var socket = io.connect('ws://localhost:80');
+    socket.on('my response', function(msg) {
+        $('#log').append('<p>Received: ' + msg.data + '</p>');
+    });
+    $('form#emit').submit(function(event) {
+        socket.emit('my event', {data: $('#emit_data').val()});
+        return false;
+    });
+    $('form#broadcast').submit(function(event) {
+        socket.emit('my broadcast event', {data: $('#broadcast_data').val()});
+        return false;
+    });
