2c8197691b9c652e12e945a4ea8371c858ea38fe
Ben Commet <bencommet@gmail.com>
2017-04-05 18:12:48 -0400

    added lives counter, game now exits gracefully

--- moleThread.py
+++ moleThread.py
@@ -5,28 +5,33 @@ import pygame
 from pygame.locals import *
 
 '''
-This class creates a thread that builds on the threading.Thread class. It adds
+* This class creates a thread that builds on the threading.Thread class. It adds
 methods that handle popping up moles and putting them down.
 '''
 class MoleThread(threading.Thread):
     '''
-    Constructor of the class
+    * Constructor of the class
     @param{int} moleNum - This integer represents the position of the moleNum
     @param{semaphore} - This semaphore is instantiatied with the maximum number
     of moles that can be popped up at once
     @param{sprite} - this is a pygame sprite object, we need it to manipulate what
     the user sees
     '''
-    def __init__(self, moleNum, semaphore, moleSprite):
+    def __init__(self, moleNum, molesUp, moleSprite, lifeGroup, livesSemaphore, livesRemaining, lives, deathGroup):
         threading.Thread.__init__(self)
         self.moleNum = moleNum
-        self.semaphore = semaphore
+        self.molesUp = molesUp
         self.moleSprite = moleSprite
+        self.lifeGroup = lifeGroup
+        self.livesSemaphore = livesSemaphore
+        self.livesRemaining = livesRemaining
+        self.deathGroup = deathGroup
+        self.lives = lives
         self.frames = ['empty_hole', 'mole_up.png']
         self.animationTime = 50
 
     '''
-    This function is called on startup of a thread. It will sleep for a random
+    * This function is called on startup of a thread. It will sleep for a random
     period of time and then call the moleUp function
     '''
     def run(self):
@@ -35,28 +40,36 @@ class MoleThread(threading.Thread):
         self.moleUp()
 
     '''
-    This function is called to kill off a thread
+    *
     '''
-    def stop(self):
-        print('stopping')
-        return 0
+    def removeLife(self):
+        currentLife = self.lives.pop()
+        self.deathGroup.add(currentLife)
+        self.lifeGroup.remove(currentLife)
+        currentLife.image = pygame.image.load("tiny_mole_fade.png").convert_alpha()
 
     '''
-    This function acquires from a semaphore and changes the appearance of a mole
-    to reflect the fact that is now up. A random period of time is generated
+    * This function acquires from the molesUp semaphore and changes the appearance of
+    a mole to reflect the fact that is now up. A random period of time is generated
     '''
     def moleUp(self):
-        self.semaphore.acquire()
+        self.molesUp.acquire()
         self.moleSprite.isUp = True
         self.moleSprite.image = pygame.image.load("mole_up.png").convert_alpha()
         time.sleep(random.random()*4 + 1)
         self.moleDown()
 
     '''
-    
+    * This function will make the mole go down
     '''
     def moleDown(self):
-        self.semaphore.release()
+        self.molesUp.release()
         if self.moleSprite.isUp:
             self.moleSprite.image = pygame.image.load("empty.png").convert_alpha()
+        print("feesh")
+        if self.moleSprite.isUp:
+            print('aquiring')
+            self.livesSemaphore.acquire()
+            self.removeLife()
         self.run()
+        
--- pythonGame.py
+++ pythonGame.py
@@ -12,9 +12,12 @@ windowWidth = 1300
 windowHeight = 680
 imageWidth = 106
 imageHeight = 116
+lifeWidth = 27
+lifeHeight = 29
 grass_color = (0,130,64)
 locations = []
 threads = []
+lives = []
 for i in range(0, (int)(math.floor(windowHeight / (10 +imageHeight)))):
     for j in range(0, (int)(math.floor(windowWidth/ (10 + imageWidth)))):
         locations.append([j * (10 + imageWidth), i * (10 + imageHeight)])
@@ -24,8 +27,14 @@ def signal_handler(signal, frame):
     for i in range(0, len(threads) - 1):
         threads[i].stop()
     sys.exit(0)
-
-def startThreads(numberOfMoles, semaphore, _moleGroup):
+'''
+* This function will create all the threads and mole sprites we need for the game
+@param{numberOfMoles} - The total number of threads/moles we will be creating
+@param{semaphore} - This semaphore is instantiatied with the maximum number
+of moles that can be popped up at once
+@param{pygame sprite group} - this group holds all the mole sprites
+'''
+def startThreads(numberOfMoles, semaphore, _moleGroup, lifeGroup, livesSemaphore, livesRemaining, deathGroup):
     threads = []
     for i in range(numberOfMoles):
         location = locations[random.randint(0, len(locations) - 1)]
@@ -35,72 +44,114 @@ def startThreads(numberOfMoles, semaphore, _moleGroup):
         threadMole.image = pygame.image.load("empty.png").convert_alpha()
         threadMole.rect = Rect(location[0], location[1], imageWidth, imageHeight)
         _moleGroup.add(threadMole)
-        thread = MoleThread(i, semaphore, threadMole)
+        thread = MoleThread(i, semaphore, threadMole, lifeGroup, livesSemaphore, livesRemaining, lives, deathGroup)
+        thread.setDaemon(True)
         thread.start()
     return threads
 
+'''
+*
+'''
+def makeLives():
+    startX = windowWidth - (lifeWidth + 5) * 10
+    yPos = windowHeight - lifeHeight - 5
+    returnGroup = pygame.sprite.Group()
+    for i in range (0, 9):
+        life = pygame.sprite.Sprite()
+        life.rect = Rect(startX + i * lifeWidth, yPos, lifeWidth, lifeHeight)
+        life.image = pygame.image.load("tiny_mole.png")
+        returnGroup.add(life)
+        lives.append(life)
+
+    return returnGroup
+
+'''
+* This function creates the pygame object and handles the main game loop
+@param{numberOfMoles} - maximum number of moles/threads we will make
+@param{semaphore} - this semaphore prevents us from going showing too many moles
+at one time.
+'''
 def game(numberOfMoles, semaphore):
+    livesSemaphore = threading.BoundedSemaphore(9)
+    livesRemaining = 10
     playerScore = 0
     # initialize game engine
     pygame.init()
     # set screen width/height and caption
     size = [windowWidth, windowHeight]
     screen = pygame.display.set_mode(size)
+
     pygame.display.set_caption('My Game')
+    myfont = pygame.font.SysFont("monospace", 48)
 
     moleGroup = pygame.sprite.Group()
+    # Create lives
+    lifeGroup = makeLives()
+    deathGroup = pygame.sprite.Group()
     # initialize clock. used later in the loop.
     clock = pygame.time.Clock()
 
-    threads = startThreads(numberOfMoles, semaphore, moleGroup)
+    threads = startThreads(numberOfMoles, semaphore, moleGroup, lifeGroup, livesSemaphore, livesRemaining, deathGroup)
     print('you made it!')
     # Loop until the user clicks close button
     done = False
     while done == False:
+        scoretext = myfont.render("Score = "+str(playerScore), 1, (0,0,0))
         # write event handlers here
         x = 0
         y = 0
         for event in pygame.event.get():
             if event.type == pygame.MOUSEBUTTONUP: x,y = event.pos
             if event.type == pygame.QUIT:
-                for i in range(0, len(threads) - 1):
-                    threads[i].stop()
+                # for i in range(0, len(threads) - 1):
+                #     threads[i].stop()
                 done = True
         # write game logic here
-
+        if(len(lives) == 0):
+            print('rekts')
+            exit()
         for mole in moleGroup:
             if mole.rect.collidepoint(x, y):
                 if mole.isUp:
                     mole.image = pygame.image.load("empty.png").convert_alpha()
                     playerScore += 1
+                    mole.isUp = False
         # clear the screen before drawing
         screen.fill((grass_color))
         # write draw code here
         moleGroup.draw(screen)
+        lifeGroup.draw(screen)
+        deathGroup.draw(screen)
+        screen.blit(scoretext, (5, 10))
         pygame.display.update()
+
 
         clock.tick(60)
 
     # close the window and quit
     pygame.quit()
-
+'''
+* Main function for our program
+'''
 def main():
-    # Handle the command line arguments
+    # Make sure we have the correct number of arguments
     if len(sys.argv) != 3:
         print('incorrect number of arguments')
         exit(0)
 
+    # Make sure the arguments are actually numbers
     try:
         numberOfMoles = int(sys.argv[1])
         maxMoles = int(sys.argv[2])
     except ValueError:
         print('Your command line arguments should be input as follows <number of moles> <max moles at at time>\n')
 
-    #check to see if there are too many moles to display
+    # check to see if there are too many moles to display
     if numberOfMoles >= len(locations):
         print('That is too many moles to display. Please use less than ' + (str)(len(locations)) + ' moles')
         exit(1)
 
+    # make sure we aren't trying to make more threads than we have moles
     if maxMoles > numberOfMoles:
         print('Your maximum number of moles is greater than the amount of moles on screen')
 
